b72d6a84b25d75347c34f18837732911b1259ecd
nivagator <gavingreer@mac.com>
2020-02-26 21:42:15 -0600

    add ratelimit, host limits

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
+// server.js
 // make config file available
 require('dotenv').config();
 
-// server.js - load the modules
+// load the modules
 const express = require('express');
 const request = require('request');
+const helmet = require('helmet');
+const hostValidation = require('host-validation');
+const rateLimit = require('express-rate-limit');
 const app = express();
+
+// server options
+const port = process.env.PORT || 3001
+
+// add rate limiting
+// app.enable("trust proxy"); // if already behind a reverse proxy
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 20 // limit each IP to 20 requests per windowMs (5 minutes)
+});
+app.use(limiter); // apply to all requests
+
+// configure hostValidation to only accept requests from a specific host () and referer
+// if the following hosts/referers don't match the client, the request will be rejected
+app.use(hostValidation({ hosts: ['127.0.0.1',
+                                 `localhost:${port}`,
+                                 'gavingreer.com', 
+                                 /.*\.gavingreer\.com$/,
+                                 'g4v.in', 
+                                 /.*\.g4v\.in$/] })); //, referers: ['https://gavingreer.com/wx']
+
+// user default helmet rules for added security 
+app.use(helmet());
 
 // server.js - set the route
 app.get('/forecast/:lat,:long', (req,res) => {
@@ -19,7 +46,6 @@ app.get('/forecast/:lat,:long', (req,res) => {
   // send the request and direct the results back to the user
   req.pipe(request(url)).pipe(res);
 });
-// server options
-const port = process.env.PORT || 3001
+
 // app starts a server and listens on port 3000 for connections
 app.listen(port, () => console.log(`server ready and listening on port ${port}`));
